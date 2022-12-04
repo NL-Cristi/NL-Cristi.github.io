99ed7e13f00635c75927e9a45f261287b6ca6b85
Cristian Negulescu <cristian@clamsen.com>
2022-12-04 21:32:22 +0000

    migrated to AstroBuild
    migrated to AstroBuild

---
+++ src/config.ts
@@ -0,0 +1,135 @@
+import type { SocialObjects } from "./types";
+
+export const SITE = {
+  website: "https://softwaremechanic.me/",
+  author: "Cristian Negulescu",
+  desc: "A small place for my personal blog.",
+  title: "SoftwareMechanic",
+  ogImage: "yo.png",
+  lightAndDarkMode: true,
+  postPerPage: 10,
+};
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: false,
+  },
+  {
+    name: "Instagram",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/cristian-laurentiu-negulescu/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:softwaremechanic@clamsen.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/NL_Cristi",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  },
+  {
+    name: "Twitch",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://github.com/NL-Cristi",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+];
---
+++ src/contents/how-to-procdump.md
@@ -0,0 +1,29 @@
+---
+author: Cristian Negulesc
+datetime: 2022-12-04T15:22:00Z
+title: How to procdump
+slug: how-to-procdump
+featured: false
+draft: false
+tags:
+  - docs
+ogImage: ""
+description:
+  Some rules & recommendations for creating or adding new posts using AstroPaper
+  theme.
+---
+
+Use this for an easy way of when/how to use procdump
+
+Please replace ***999*** from the lower table with the PID of your process. To get the pid you can use the Details View of the TaskManager or powershel/cm.
+
+
+
+|#|Scenario|Objective|Basic syntax|
+|---|---|---|---|
+|1|**Hang** dump |Creates a dump file (process memory is saved to disk) when the tool runs |```.\procdump.exe -ma 999```|
+|2|**Crash** dump|Monitors a process and creates a dump file when the process runs into an unhandled exception|```.\procdump.exe -ma -e 999```|
+|3|Dump on **exception**|Monitors a process and creates a dump file on a specific exception (i.e. any exceptions of form *ArgumentNullException*)|```.\procdump.exe -ma -e 1 -f ArgumentNullException 999```|
+|4|Dump on **first chance exception**|Monitors a process and creates a dump file on a first chance exception|```.\procdump.exe -ma -e 1 999```|
+|5|**Monitors exceptions**|Monitors a process for exceptions without taking a dump |```.\procdump.exe -ma -e 1 -f "" 999```|
+|6|Dump on **performance trigger**|Monitors a process and takes a hang dump when a performance trigger is hit|```.\procdump.exe -ma -p <Counter> <Threshold value> <process name or service or PID>```|
